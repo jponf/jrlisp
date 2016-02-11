7d93cdf933a466427ca77eb27801c591a3cb514d
Josep Pon Farreny <jponfarreny@gmail.com>
2016-02-11 16:00:17 +0100

    EvaluateAllArgumentsStrategy implemented recursively

--- src/main/java/cat/udl/eps/butterp/data/callable/EvaluateAllArgumentsStrategy.java
+++ src/main/java/cat/udl/eps/butterp/data/callable/EvaluateAllArgumentsStrategy.java
@@ -1,23 +1,19 @@
 package cat.udl.eps.butterp.data.callable;
 
+import cat.udl.eps.butterp.data.ConsCell;
 import cat.udl.eps.butterp.data.ListOps;
 import cat.udl.eps.butterp.data.SExpression;
+import cat.udl.eps.butterp.data.Symbol;
 import cat.udl.eps.butterp.environment.Environment;
-
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 public class EvaluateAllArgumentsStrategy implements ArgumentsEvaluationStrategy {
 
     @Override
     public SExpression evaluateArguments(SExpression args, Environment env) {
-        List<SExpression> evargs = new ArrayList<>();
-        Iterator<SExpression> it = ListOps.createIterator(args);
-        while (it.hasNext()) {
-            evargs.add(it.next().eval(env));
+        if (Symbol.NIL.equals(args)) {
+            return args;
         }
 
-        return ListOps.list(evargs);
+        return new ConsCell(ListOps.car(args).eval(env), evaluateArguments(ListOps.cdr(args), env));
     }
 }
